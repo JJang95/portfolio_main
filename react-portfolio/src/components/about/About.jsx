d50c812ebd58848813469d2f85b590e7d4bba59a
JJang95 <jojang1994@gmail.com>
2023-03-02 16:10:17 -0500

    Added about jsx component

--- react-portfolio/src/components/about/About.jsx
+++ react-portfolio/src/components/about/About.jsx
@@ -1,9 +1,54 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
+
 
 const About = () => {
     return (
-        <section id='about'>About</section>
+        <section id='about'>
+            <h5>Get To Know</h5>
+            <h2>About Me</h2>
+
+            <div className="container about__container">
+                <div className="about__me">
+                    <div className="about__me-image">
+                        <img src={ME} alt="About Image" />
+                    </div>
+                </div>
+
+                <div className="about__content">
+                    <div className="about__cards">
+                        <article className='about__card'>
+                            <FaAward className='about__icon' />
+                            <h5>Experience</h5>
+                            <small>2+ Years of programming</small>
+                        </article>
+
+                        <article className='about__card'>
+                            <FiUsers className='about__icon' />
+                            <h5>Clients</h5>
+                            <small>5+ Small businesses</small>
+                        </article>
+
+                        <article className='about__card'>
+                            <VscFolderLibrary className='about__icon' />
+                            <h5>Projects</h5>
+                            <small>10+ personal projects utilizing full-stack languages</small>
+                        </article>
+                    </div>
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est similique veritatis incidunt quas nobis 
+                        sit accusantium commodi, odit consequatur minus, at soluta? Voluptate perferendis odit, nostrum rem 
+                        eligendi expedita molestiae?
+                    </p>
+
+                    <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+                </div>
+            </div>
+        </section>
     )
 }
 
