10da9337348023945d3bd319e9e596c9e66e2ae9
JJang95 <jojang1994@gmail.com>
2023-03-04 17:23:54 -0500

    Finished integrating emailJS with portfolio

--- react-portfolio/src/components/contact/Contact.jsx
+++ react-portfolio/src/components/contact/Contact.jsx
@@ -3,8 +3,26 @@ import './contact.css'
 import {MdOutlineMailOutline} from 'react-icons/md'
 import {RiLinkedinBoxFill} from 'react-icons/ri'
 import {BsTelephoneFill} from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_fk6dfhg', 'template_dl9nmpq', form.current, 'cU1sUbankDVDSnw45')
+
+            e.target.reset()
+
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
+
     return (
         <section id='contact'>
             <h5>Get In Touch</h5>
@@ -33,7 +51,7 @@ const Contact = () => {
                     </article>
                 </div>
 
-                <form action="">
+                <form ref={form} onSubmit={sendEmail}>
                     <input type="text" name='name' placeholder='Your Full Name' required/>
                     <input type="email" name='email' placeholder='Your Email' required/>
                     <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
