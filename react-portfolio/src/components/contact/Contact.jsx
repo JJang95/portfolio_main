68973e1bc57c97e5b794f46595f6ea1a39a09034
JJang95 <jojang1994@gmail.com>
2023-03-04 16:02:31 -0500

    Finished Contact CSS/responsive design

--- react-portfolio/src/components/contact/Contact.jsx
+++ react-portfolio/src/components/contact/Contact.jsx
@@ -12,22 +12,22 @@ const Contact = () => {
 
             <div className="container contact__container">
                 <div className="contact__options">
-                    <article className="contract__option">
-                        <MdOutlineMailOutline/>
+                    <article className="contact__option">
+                        <MdOutlineMailOutline className='contact__option-icon'/>
                         <h4>Email</h4>
                         <h5>JJ299@njit.edu</h5>
                         <a href="mailto:JJ299@njit.edu" target="_blank" rel="noreferrer">Send a message</a>
                     </article>
 
-                    <article className="contract__option">
-                        <RiLinkedinBoxFill/>
+                    <article className="contact__option">
+                        <RiLinkedinBoxFill className='contact__option-icon'/>
                         <h4>Linkedin</h4>
                         <h5>Jonathan Jang</h5>
                         <a href="https://www.linkedin.com/in/jonathan-jang-08a308168/" target="_blank" rel="noreferrer">Connect with me</a>
                     </article>
 
-                    <article className="contract__option">
-                        <BsTelephoneFill/>
+                    <article className="contact__option">
+                        <BsTelephoneFill className='contact__option-icon'/>
                         <h4>Phone Number</h4>
                         <h5>732-354-9388</h5>
                     </article>
